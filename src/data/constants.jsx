7171fd40869bf173843f2e9ea82dd79ba735ee4d
RezkyPurnama <rezkypurnama35@gmail.com>
2024-11-05 15:34:33 +0700

    Initial commit

--- src/data/constants.jsx
+++ src/data/constants.jsx
@@ -1,121 +1,99 @@
+import picture from "../assets/onlineshopp.png";
+import picture1 from "../assets/inventori.png";
+import picture2 from "../assets/sekolahsaya1.png";
+
 export const Bio = {
-    name: "Rezky Purnama Sarta",
-    roles: [
+  name: "Rezky Purnama Sarta",
+  roles: ["Front-End Developer", "UI/UX Designer"],
+  description:
+    "Saya adalah individu yang termotivasi dan serba bisa, selalu bersemangat untuk menghadapi tantangan baru. Dengan semangat belajar, saya berdedikasi untuk memberikan hasil berkualitas tinggi. Dengan sikap positif dan pola pikir berkembang, saya siap memberikan kontribusi yang berarti dan mencapai hal-hal besar.",
+  github: "https://github.com/RezkyPurnama",
+  insta: "https://www.instagram.com/_rezkypurnama/",
+  email: "",
+};
 
-      "Front-End Developer",
-      "UI/UX Designer",
-    ],
-    description:
-      "Saya adalah individu yang termotivasi dan serba bisa, selalu bersemangat untuk menghadapi tantangan baru. Dengan semangat belajar, saya berdedikasi untuk memberikan hasil berkualitas tinggi. Dengan sikap positif dan pola pikir berkembang, saya siap memberikan kontribusi yang berarti dan mencapai hal-hal besar.",
-    github: "https://github.com/RezkyPurnama",
-    insta: "https://www.instagram.com/_rezkypurnama/",
-    email: "",
-  };
-  
-  export const skills = [
-    {
-      skills: [
-        {
-          name: "React Js",
-          image:
-            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
-        },
-        {
-          name: "HTML",
-          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
-        },
-        {
-          name: "CSS",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
-        },
-        {
-          name: "JavaScript",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
-        },   
-        {
-          name: "Flutter",
-          image:
-            "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
-        },
-        {
-          name: "MySQL",
-          image:
-            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
-        },
-      ],
-    },
-  ];
-  
-  export const experiences = [
-    {
-      id: 0,
-      role: " Website Inventori Jurusan Teknologi Informasi", 
-      desc: "WEB Inventori Jurusan Teknologi Informasi merupakan tugas Project Base Learning (PBL) yang dikerjakan dalam bentuk tim. Web ini bertujuan untuk meningkatkan efisiensi dan akurasi manajemen stok barang di Jurusan Teknologi Informasi. Website ini menawarkan fitur pernantauan persediaan barang, dan pengelolaan data peminjaman, sehingga proses manajemen stok menjadi lebih terorganisir dan efisien.",
-      skills: [
-        "PHP",
-        "HTML",
-        "CSS",
-        "JavaScript",
-      ],
-
-    },
-    {
-      id: 0,
-      role: "Website SMAN 14 Padang ",
-      desc: "Website SMAN 14 Padang dibangun dengan Bootstrap untuk menyediakan platform yang responsif dan Informatif bagi komunitas sekolah. Situs ini menyajikan profil lengkap, termasuk struktur organisasi, visi dan misi, dan juga menampilkan galeri kegiatan dan prestasi siswa, serta contact yang dapat dihubungi. Dengan desain yang user-friendly, website ini mempermudah komunikasi antara siswa, orang tua, dan masyarakat.",
-      skills: [
-        "HTML",
-        "CSS",
-        "JavaScript",
-      ],
+export const skills = [
+  {
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
       },
-    {
-      id: 1,
-      role: "Mobile OnlineShopRezky",
-      desc: "Pengembangan aplikasi onlineshop berbasis mobile menggunakan Flutter untuk memberikan pengalaman belanja yang nyaman melalui antarmuka intuitif. Fitur utama termasuk CRUD untuk daftar produk, melihat detail produk.Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
-      skills: [
-        "Flutter",
-      ],
-    },
-  
-  ];
-  
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+    ],
+  },
+];
 
-  
-  export const projects = [
-    {
-      id: 4,
-      title: "Website Inventori Jurusan Teknologi Informasi",
-      description:
-        "proyek berbasis tim yang bertujuan meningkatkan efisiensi dan akurasi manajemen stok di jurusan tersebut. Website ini menyediakan fitur pemantauan stok dan pengelolaan data peminjaman, menjadikan proses manajemen inventori lebih terstruktur dan efisien.",
-      image:
-        "https://private-user-images.githubusercontent.com/187028228/383050522-8c8e2189-ab79-4588-8606-65ba20fae2ab.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzA3OTI4NjEsIm5iZiI6MTczMDc5MjU2MSwicGF0aCI6Ii8xODcwMjgyMjgvMzgzMDUwNTIyLThjOGUyMTg5LWFiNzktNDU4OC04NjA2LTY1YmEyMGZhZTJhYi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQxMTA1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTEwNVQwNzQyNDFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1iNWQ0YjljZmIzYTk0MjNhMTIzNDZiYzVjYjYyMWE1NjRhYWE2YTVmNTZiN2EyNzE4ZDgzZDkyMzA0MDlkOTFiJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.OhEmYFfcmf31y_7OFH7ypN0kN4PFfVJc7UwvhLcKejU",
-      
-    },
-    {
-      id: 5,
-      title: "Website SMAN 14 Padang",
-    
-      description:
-        "Website SMAN 14 Padang dibangun dengan Bootstrap untuk menyediakan platform yang responsif dan Informatif bagi komunitas sekolah. Situs ini menyajikan profil lengkap, termasuk struktur organisasi, visi dan misi, dan juga menampilkan galeri kegiatan dan prestasi siswa, serta contact yang dapat dihubungi. Dengan desain yang user-friendly.",
-      image:
-        "https://private-user-images.githubusercontent.com/187028228/383050293-dd4ca68d-4a4e-4804-9ad9-8c83b7f7276d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzA3OTI4NjEsIm5iZiI6MTczMDc5MjU2MSwicGF0aCI6Ii8xODcwMjgyMjgvMzgzMDUwMjkzLWRkNGNhNjhkLTRhNGUtNDgwNC05YWQ5LThjODNiN2Y3Mjc2ZC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQxMTA1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTEwNVQwNzQyNDFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02Y2U3ZWI4OTM0Y2RiN2RkYTI2NWViOGEzZWE3NmNiODFiN2M5NTVkNTk2Zjc5NjQ1NTNlMzkwZGEzZTkxNWVhJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.w_Q6TGS83z2x2WNiTAc2x8JhG2wxDJfNoUgbky_8v5Y",
-      
-    },
-    {
-      id: 6,
-      title: "Mobile OnlineShopRezky",
-     
-      description:
-        "Pengembangan aplikasi onlineshop berbasis mobile menggunakan Flutter untuk memberikan pengalaman belanja yang nyaman melalui antarmuka intuitif. Fitur utama termasuk CRUD untuk daftar produk, melihat detail produk.Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
-      image:
-        "https://private-user-images.githubusercontent.com/187028228/383050390-83167937-a93c-42f6-a810-b45a9605ef04.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzA3OTI4NjEsIm5iZiI6MTczMDc5MjU2MSwicGF0aCI6Ii8xODcwMjgyMjgvMzgzMDUwMzkwLTgzMTY3OTM3LWE5M2MtNDJmNi1hODEwLWI0NWE5NjA1ZWYwNC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQxMTA1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTEwNVQwNzQyNDFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT01NTZlZDY2OWE5MzFhNmUyNTdkZDllN2IzYTI3OTViNDIzMjhlYTBjY2QxYmNhNDVkMmI5YTMwYmZhMGRkNzUxJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.daaKsE9r1mEO-8XfTTkH0dI-vsX5tYyf-fnmFFX-H7s",
-  
-    },
- 
-  ];
-  
- 
-  
+export const experiences = [
+  {
+    id: 0,
+    role: " Website Inventori Jurusan Teknologi Informasi",
+    desc: "WEB Inventori Jurusan Teknologi Informasi merupakan tugas Project Base Learning (PBL) yang dikerjakan dalam bentuk tim. Web ini bertujuan untuk meningkatkan efisiensi dan akurasi manajemen stok barang di Jurusan Teknologi Informasi. Website ini menawarkan fitur pernantauan persediaan barang, dan pengelolaan data peminjaman, sehingga proses manajemen stok menjadi lebih terorganisir dan efisien.",
+    skills: ["PHP", "HTML", "CSS", "JavaScript"],
+  },
+  {
+    id: 0,
+    role: "Website SMAN 14 Padang ",
+    desc: "Website SMAN 14 Padang dibangun dengan Bootstrap untuk menyediakan platform yang responsif dan Informatif bagi komunitas sekolah. Situs ini menyajikan profil lengkap, termasuk struktur organisasi, visi dan misi, dan juga menampilkan galeri kegiatan dan prestasi siswa, serta contact yang dapat dihubungi. Dengan desain yang user-friendly, website ini mempermudah komunikasi antara siswa, orang tua, dan masyarakat.",
+    skills: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    id: 1,
+    role: "Mobile OnlineShopRezky",
+    desc: "Pengembangan aplikasi onlineshop berbasis mobile menggunakan Flutter untuk memberikan pengalaman belanja yang nyaman melalui antarmuka intuitif. Fitur utama termasuk CRUD untuk daftar produk, melihat detail produk.Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+    skills: ["Flutter"],
+  },
+];
+
+export const projects = [
+  {
+    id: 4,
+    title: "Website Inventori Jurusan Teknologi Informasi",
+    description:
+      "proyek berbasis tim yang bertujuan meningkatkan efisiensi dan akurasi manajemen stok di jurusan tersebut. Website ini menyediakan fitur pemantauan stok dan pengelolaan data peminjaman, menjadikan proses manajemen inventori lebih terstruktur dan efisien.",
+    image:
+      "https://private-user-images.githubusercontent.com/187028228/383065005-11febff7-286b-41b1-a6f1-d1d4ad3c0482.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzA3OTU3MzgsIm5iZiI6MTczMDc5NTQzOCwicGF0aCI6Ii8xODcwMjgyMjgvMzgzMDY1MDA1LTExZmViZmY3LTI4NmItNDFiMS1hNmYxLWQxZDRhZDNjMDQ4Mi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQxMTA1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTEwNVQwODMwMzhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0wZGEzOTk1NmI5OGE4MmUzYTE5MGIwMDMxNWY2ZDg4YTNmNmY0MTY5NzBkZmNmMDQ3ZGUzMjI2ZjU1NGIyMzFkJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.g107piN3fjzaRv1AdtoMIipAlv59Z1zX_kQvss9LC-Q",
+  },
+  {
+    id: 5,
+    title: "Website SMAN 14 Padang",
+
+    description:
+      "Website SMAN 14 Padang dibangun dengan Bootstrap untuk menyediakan platform yang responsif dan Informatif bagi komunitas sekolah. Situs ini menyajikan profil lengkap, termasuk struktur organisasi, visi dan misi, dan juga menampilkan galeri kegiatan dan prestasi siswa, serta contact yang dapat dihubungi. Dengan desain yang user-friendly.",
+    image:
+      "https://private-user-images.githubusercontent.com/187028228/383050293-dd4ca68d-4a4e-4804-9ad9-8c83b7f7276d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzA3OTUxMTcsIm5iZiI6MTczMDc5NDgxNywicGF0aCI6Ii8xODcwMjgyMjgvMzgzMDUwMjkzLWRkNGNhNjhkLTRhNGUtNDgwNC05YWQ5LThjODNiN2Y3Mjc2ZC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQxMTA1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTEwNVQwODIwMTdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT00YTNjNWEyNDkwMWQ1YjVhODM0ODhhMmViYmZiN2FiYmZkMTU5NjY4ZjJiYWVhYjNiYjY0MzMwNWVjOWJkMzNkJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.wFaocqtBedPVHjADYbxsNHtzGWYfsDIBQK1sJkxBcdY",
+  },
+  {
+    id: 6,
+    title: "Mobile OnlineShopRezky",
+
+    description:
+      "Pengembangan aplikasi onlineshop berbasis mobile menggunakan Flutter untuk memberikan pengalaman belanja yang nyaman melalui antarmuka intuitif. Fitur utama termasuk CRUD untuk daftar produk, melihat detail produk.Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+    image:
+      "https://private-user-images.githubusercontent.com/187028228/383065022-64dc6a01-0218-4862-9447-dde2804017c2.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzA3OTU3MzgsIm5iZiI6MTczMDc5NTQzOCwicGF0aCI6Ii8xODcwMjgyMjgvMzgzMDY1MDIyLTY0ZGM2YTAxLTAyMTgtNDg2Mi05NDQ3LWRkZTI4MDQwMTdjMi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQxMTA1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTEwNVQwODMwMzhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yODU5NmViZGYxMTQ2Nzc5YjA4NTM2NTM2M2Y4NTI2YjA2YzEyNzI5NWFlZTI4YTBmZmRjM2M4MGI5ZTUxZjcyJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.UWMiat8L_SWVKo1rs6N5Hr0nUSi9jYuVHGcbAmwkE-0",
+  },
+];
